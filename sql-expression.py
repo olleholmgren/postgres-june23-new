bca86e297c41a22ab2b15b8c27b24826f369b121
olleholmgren <olle.holmgren@gmail.com>
2023-05-30 14:23:07 +0000

    4

--- sql-expression.py
+++ sql-expression.py
@@ -1,3 +1,7 @@
 from sqlalchemy import (
     create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
 )
+
+# executing the instructions from our localhost "chinook" db
+
+db = create_engine("postgresql:///chinook")
